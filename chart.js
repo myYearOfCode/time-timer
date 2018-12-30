8efc062bbf6674175436bd942ea4f85485954949
Ross <Ross@myyearofcode.com>
2018-12-29 21:47:39 -0500

    add smooth opacity fade after time is up.

--- chart.js
+++ chart.js
@@ -10,50 +10,61 @@ data = {
           'rgba(255, 0, 0, 1)',
           'rgba(255, 255, 255, 1)'
         ],
+        // borderColor: 'rbga(255, 0, 0, 1)',
         borderWidth: 0,
 
     }],
+
     // These labels appear in the legend and in the tooltips when hovering different arcs
     // labels: [
     //     'Red',
     //     'White'
     // ]
 };
+Chart.defaults.global.legend.display = false;
 
 // ask for user input on timer length
 let duration = prompt("how many seconds would you like to wait?");
 var start = Date.parse(new Date());
 var end = start + (duration * 1000);
-
+let finished = false;
+let animation_duration = 1000;
 // For a pie chart
 var myPieChart = new Chart(ctx,{
     type: 'pie',
     data: data,
     options: {
       animation: {
-        duration: 1000, //keep this == to the setInterval timeout
+        duration: animation_duration, //keep this == to the setInterval timeout
         easing: 'linear'
       },
+      events: [],
 
-    layout: {
-          circumference: 1,
-          padding: {
-              left: 50,
-              right:50,
-              top: 50,
-              bottom: 50
-          }
+      layout: {
+        circumference: 1,
+        padding: {
+            left: 50,
+            right:50,
+            top: 50,
+            bottom: 50
+        }
       }
     }
 });
 
 function changeData() {
-  if (myPieChart.data.datasets[0].data[0] <100){
+  if (myPieChart.data.datasets[0].data[0] <100 && finished == false){
     let newPercent = time_based(start, end);
     myPieChart.data.datasets[0].data[0] = newPercent;
     myPieChart.data.datasets[0].data[1] = 100-newPercent;
     myPieChart.update();
     // time_based(start, end);
+  }
+  if (myPieChart.data.datasets[0].data[0] == 100 && finished == false){
+    console.log('setting class to finished');
+    finished = true;
+    let element = document.querySelector(".chart");
+    element.classList.add("finished")
   }
 }
 
@@ -64,4 +75,4 @@ function time_based(start, end) {
   return percent;
 }
 
-setInterval(changeData, 1000) // keep this == to the animation timeout
+setInterval(changeData, animation_duration) // keep this == to the animation timeout
