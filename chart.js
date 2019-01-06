51291c9527106757776a588619277395dfb2e7ba
Ross <Ross@myyearofcode.com>
2019-01-05 22:51:46 -0500

    add buttons for start timer and pingpong

--- chart.js
+++ chart.js
@@ -23,20 +23,55 @@ data = {
 };
 Chart.defaults.global.legend.display = false;
 
+// I would like to have form elements for
+// x toggle pingpong on off
+// set interval
+// pause timer
+// x start!
+
+
+function startRunning(){
+  duration = prompt("how many seconds would you like to wait?");
+  active = true;
+  start = Date.parse(new Date());
+  end = start + (duration * 1000);
+  countingUp = setInterval(countUp, animation_duration) // keep this == to the animation timeout
+}
+
+function togglePingPong(){
+  pingPong = !pingPong;
+  console.log("pingPong is "+ pingPong + "\n");
+}
+
+function init(){
+document.getElementById("startButton").addEventListener('click', function(){ startRunning(); return false; });
+document.getElementById("togglePingPong").addEventListener('click', function(){ togglePingPong(); return false; });
+//     // document.getElementById("form").addEventListener("click", function(event){
+//     //   event.preventDefault()
+//     // });
+//     document.getElementById('form').onsubmit = startRunning;
+    console.log('hi')
+}
+//
+window.onload = init;
+
 // ask for user input on timer length
-let duration = prompt("how many seconds would you like to wait?");
+let duration = ""
+let active = false;
 var start = Date.parse(new Date());
+let pingPong = false;
 var end = start + (duration * 1000);
 let finished = false;
 let countType = true;
 let animation_duration = 1000;
+var countingUp = "";
 // For a pie chart
 var myPieChart = new Chart(ctx,{
     type: 'pie',
     data: data,
     options: {
       animation: {
-        duration: animation_duration, //keep this == to the setInterval timeout
+        duration: animation_duration, // keep this == to the setInterval timeout
         easing: 'linear'
       },
       events: [],
@@ -54,20 +89,20 @@ var myPieChart = new Chart(ctx,{
 });
 
 function countUp() {
+  console.log("countup")
   myPieChart.data.datasets[0].backgroundColor = [
     'rgba(255, 0, 0, 1)',
     'rgba(255, 255, 255, 1)'
   ]
   if (myPieChart.data.datasets[0].data[0] <100){
-    let newPercent = time_based(start, end);
+    let newPercent = time_percent(start, end);
     myPieChart.data.datasets[0].data[0] = newPercent;
     myPieChart.data.datasets[0].data[1] = 100-newPercent;
     myPieChart.update();
-
   }
 
-  if (myPieChart.data.datasets[0].data[0] == 100){
-    console.log('count up. setting class to finished');
+  if (myPieChart.data.datasets[0].data[0] == 100 && pingPong){
+    console.log('done counting up.');
     countType = !countType;
     resetData()
   }
@@ -78,15 +113,14 @@ function countDown() {
     'rgba(0, 255, 0, 1)',
     'rgba(255, 255, 255, 1)'
   ]
-
   if (myPieChart.data.datasets[0].data[0] != 0){
-    let newPercent = time_based(start, end);
+    let newPercent = time_percent(start, end);
     myPieChart.data.datasets[0].data[0] = 100-newPercent;
     myPieChart.data.datasets[0].data[1] = newPercent;
     myPieChart.update();
   }
-  if (myPieChart.data.datasets[0].data[0] == 0){
-    console.log('countdown. setting class to finished');
+  if (myPieChart.data.datasets[0].data[0] == 0  && pingPong){
+    console.log('done counting down.');
     countType = !countType;
     resetData()
   }
@@ -105,12 +139,10 @@ function resetData() {
     }
 }
 
-function time_based(start, end) {
+// this returns a % of the time period
+function time_percent(start, end) {
   let now = Date.parse(new Date());
   let percent = ((now-start) / (end-start))*100;
   console.log("%s%%",percent.toFixed(2));
   return percent;
 }
-
-let countingUp = setInterval(countUp, animation_duration) // keep this == to the animation timeout
-// let countingDown = setInterval(countDown, animation_duration)
