7574d76b77eb7f78d20304ed571b5c570aedb62b
Ross <Ross@myyearofcode.com>
2019-01-05 21:07:34 -0500

    pingponging is on

--- chart.js
+++ chart.js
@@ -28,6 +28,7 @@ let duration = prompt("how many seconds would you like to wait?");
 var start = Date.parse(new Date());
 var end = start + (duration * 1000);
 let finished = false;
+let countType = true;
 let animation_duration = 1000;
 // For a pie chart
 var myPieChart = new Chart(ctx,{
@@ -52,7 +53,7 @@ var myPieChart = new Chart(ctx,{
     }
 });
 
-function changeData() {
+function countUp() {
   if (myPieChart.data.datasets[0].data[0] <100 && finished == false){
     let newPercent = time_based(start, end);
     myPieChart.data.datasets[0].data[0] = newPercent;
@@ -60,18 +61,53 @@ function changeData() {
     myPieChart.update();
     // time_based(start, end);
   }
-  if (myPieChart.data.datasets[0].data[0] == 100 && finished == false){
-    console.log('setting class to finished');
-    finished = true;
-    let element = document.querySelector(".chart");
-    element.classList.add("finished")
+  if (myPieChart.data.datasets[0].data[0] == 100){
+    console.log('count up. setting class to finished');
+    // finished = true;
+    countType = !countType;
+    // let element = document.querySelector(".chart");
+    // element.classList.add("finished")
+    resetData()
+  }
+}
+
+function countDown() {
+  let element = document.querySelector(".chart");
+  element.classList.remove("finished")
+  console.log(myPieChart.data.datasets[0].data[0])
+  if (myPieChart.data.datasets[0].data[0] != 0){
+    let newPercent = time_based(start, end);
+    myPieChart.data.datasets[0].data[0] = 100-newPercent;
+    myPieChart.data.datasets[0].data[1] = newPercent;
+    myPieChart.update();
+    // time_based(start, end);
+  }
+  if (myPieChart.data.datasets[0].data[0] == 0 && finished == false){
+    console.log('countdown. setting class to finished');
+    // finished = true;
+    countType = !countType;
+    // let element = document.querySelector(".chart");
+    // element.classList.add("finished")
+    resetData()
   }
 }
 
 function resetData() {
-    myPieChart.data.datasets[0].data[0] = 0;
-    myPieChart.data.datasets[0].data[1] = 100;
-    myPieChart.update();
+    // myPieChart.data.datasets[0].data[0] = 0;
+    // myPieChart.data.datasets[0].data[1] = 100;
+    // myPieChart.update();
+    start = Date.parse(new Date());
+    end = start + (duration * 1000);
+    // let finished = false;
+    if (countType){
+      clearInterval(countingDown);
+      countingUp = setInterval(countUp, animation_duration)
+    }
+    else {
+      clearInterval(countingUp);
+      countingDown = setInterval(countDown, animation_duration)
+
+    }
 }
 
 function time_based(start, end) {
@@ -81,4 +117,5 @@ function time_based(start, end) {
   return percent;
 }
 
-setInterval(changeData, animation_duration) // keep this == to the animation timeout
+let countingUp = setInterval(countUp, animation_duration) // keep this == to the animation timeout
+// let countingDown = setInterval(countDown, animation_duration)
